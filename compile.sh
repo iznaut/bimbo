7f4c7d01f96f881a9db9f9281db3c2498aa209d5
izzy kestrel <36773942+iznaut@users.noreply.github.com>
2025-02-27 11:02:06 -0600

    binary build and test action

--- compile.sh
+++ compile.sh
@@ -11,14 +11,14 @@ declare -A targets
 targets[win]="bun-windows-x64"
 targets[mac]="bun-darwin-arm64"
 targets[mac-intel]="bun-darwin-x64"
-targets[linux-gnu]="bun-linux-x64"
+targets[linux]="bun-linux-x64"
 
-# for key in "${!targets[@]}"; do
-#     bun build $MAIN_JS --compile --minify --sourcemap --bytecode --target=${targets[${key}]} --outfile $BIN_DIR/$OUTFILE_PREFIX-$key
-# done
+for key in "${!targets[@]}"; do
+    bun build $MAIN_JS --compile --minify --sourcemap --bytecode --target=${targets[${key}]} --outfile $BIN_DIR/$OUTFILE_PREFIX-$key
+done
 
-bun build $MAIN_JS --compile --minify --sourcemap --bytecode --target=${targets[$OSTYPE]} --outfile $BIN_DIR/$OUTFILE_PREFIX-$OSTYPE
-
+# build only for current OS
+# bun build $MAIN_JS --compile --minify --sourcemap --bytecode --target=${targets[$OSTYPE]} --outfile $BIN_DIR/$OUTFILE_PREFIX-$OSTYPE
 
 # create example project zip
 pushd $EXAMPLE_PROJECT_DIR
@@ -28,14 +28,8 @@ zip -r ../$BIN_DIR/example.zip *
 popd
 
 # uncomment for testing
-# cp ./bin/bimbo-mac $TEST_TARGET_DIR/bimbo-mac
-# cp ./bin/example.zip $TEST_TARGET_DIR/example.zip
-
-cd $BIN_DIR
-mkdir $TEST_TARGET_DIR
-rm -rf $TEST_TARGET_DIR/*
-cp ./example.zip $TEST_TARGET_DIR/example.zip
-./bimbo-$OSTYPE --path $TEST_TARGET_DIR
-
-# sleep 3
-# echo "testing" >> "./content/index.md"
+# cd $BIN_DIR
+# mkdir $TEST_TARGET_DIR
+# rm -rf $TEST_TARGET_DIR/*
+# cp ./example.zip $TEST_TARGET_DIR/example.zip
+# ./bimbo-$OSTYPE --path $TEST_TARGET_DIR

631c6637778ad0cc51af15f5389393685d6984b3
izzy kestrel <git@iznaut.com>
2026-01-06 16:37:53 -0600

    build scripts

---
+++ build.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env pwsh
+
+git pull
+npx @electron/packager . --extra-resource project-starters/ --ignore project-starters/ --overwrite --out build/
+butler push build/bimbo-win32-x64 iznaut/bimbo:windows-beta
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npx @electron/packager . --extra-resource project-starters/ --ignore project-starters/ --overwrite --out build/
+butler push build/bimbo-darwin-arm64 iznaut/bimbo:mac-beta
